f078198c388d9fe1fb3cb42253796f7f03677a50
Mahesh <harrydroid@gmail.com>
2015-03-06 19:46:11 -0600

    Initial Commit

--- app/src/main/java/listviewex/example/com/androidtutorials/HomeActivity.java
+++ app/src/main/java/listviewex/example/com/androidtutorials/HomeActivity.java
@@ -1,9 +1,11 @@
 package listviewex.example.com.androidtutorials;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void startListViewEx(View view){
+        Intent intent = new Intent(this, ListViewExample.class);
+        startActivity(intent);
     }
 
 
